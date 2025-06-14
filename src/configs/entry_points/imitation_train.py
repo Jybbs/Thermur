d6806aead7aaec8036e09ff8408a38fc64730837
James Parkington <pongs.acreage.0o@icloud.com>
2025-06-14 17:16:07 -0400

    refactor(architecture): prefer `zen()` instead of `SI` interpolation in `builds`

--- src/configs/entry_points/imitation_train.py
+++ src/configs/entry_points/imitation_train.py
@@ -7,17 +7,17 @@ configuration for the training script.
 """
 from configs.builds import *
 from configs.models import *
-from hydra_zen      import make_config, store
+from hydra_zen      import make_config, store, zen
 
 
 imitation_train_config = make_config(
 
-    # Training hyperparameters
-    hyperparameters = HyperparameterModel,
-    collector       = CollectorModel,
-    replay_buffer   = ReplayBufferModel,
-    logging         = LoggingModel,
-    wandb           = WandbModel,
+    # Training hyperparameters wrapped with zen
+    hyperparameters = zen(HyperparameterModel),
+    collector       = zen(CollectorModel),
+    replay_buffer   = zen(ReplayBufferModel),
+    logging         = zen(LoggingModel),
+    wandb           = zen(WandbModel),
     
     # Component builders
     environment       = build_environment,
