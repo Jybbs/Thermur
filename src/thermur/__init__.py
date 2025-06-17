be20701e12565081da100a2b9f21898ec3b1954e
James Parkington <pongs.acreage.0o@icloud.com>
2025-06-17 17:56:29 -0400

    refactor(app): reorder imports in main `__init__`

--- src/thermur/__init__.py
+++ src/thermur/__init__.py
@@ -4,41 +4,38 @@ The Thermur package: Core business logic and functionality.
 This package provides a clean API for accessing all major components of the
 Thermur system for imitation learning training.
 """
-__version__ = "0.1.0"
-
 from .cli        import app
+from .control    import ExpertFlockingController
 from .models     import GNNPolicy
 from .simulation import SwarmData, SwarmDataSpec, SimulationEnv, compute_edge_index
-from .control    import ExpertFlockingController
-from .utils      import EnvironmentDataSource, configure_loguru, set_seed
 from .training   import train_imitation_learning, save_checkpoint, ImitationLoss
+from .utils      import EnvironmentDataSource, configure_loguru, set_seed
 
 __all__ = [
-    # Version
-    "__version__",
+
+    # CLI components
+    "app",
+
+    # Control components
+    "ExpertFlockingController",
+
+    # Models
+    "GNNPolicy",
 
     # Simulation components
     "SwarmData", 
     "SwarmDataSpec",
     "SimulationEnv",
     "compute_edge_index",
-    
-    # CLI components
-    "app",
 
-    # Models
-    "GNNPolicy",
+    # Training
+    "train_imitation_learning",
+    "save_checkpoint",
+    "ImitationLoss",
 
     # Utilities
     "EnvironmentDataSource",
     "configure_loguru",
     "set_seed",
 
-    # Control components
-    "ExpertFlockingController",
-
-    # Training
-    "train_imitation_learning",
-    "save_checkpoint",
-    "ImitationLoss",
 ]
